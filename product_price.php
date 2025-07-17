a928961e85af57e438518009ddfaf3dcfe9586bf
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-07-17 22:42:04 +0530

    Product price calculation

---
+++ product_price.php
@@ -0,0 +1,16 @@
+<?php
+require('connection.inc.php');
+require('functions.inc.php');
+
+$pid = get_safe_value($con,$_POST['pro_id']);
+$sid = get_safe_value($con,$_POST['size_id']);
+$cid = get_safe_value($con,$_POST['color_id']);
+$html = '';
+
+$sql = "SELECT mrp, price FROM product_attributes WHERE product_id = '$pid' AND size_id = '$sid' AND color_id = '$cid'";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_assoc($result);
+$html .= "<span class='current'>$".$row['price']."</span>
+            <span class='normal'>$".$row['mrp']."</span>";
+echo $html;
+?>
